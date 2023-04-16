998fc0dd22c52665d2b48d5eeb7ab4f609561b4f
Pegah <Prashidnia@gmail.com>
2023-04-16 00:33:43 -0400

    Updated Concerts component to use MUI table

--- src/components/Concerts.jsx
+++ src/components/Concerts.jsx
@@ -1,25 +1,54 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { Button } from '@mui/material';
+
 function Concerts({events, onBuyTickets}) {
 
     return (
         <div>
             <h3>Upcoming Shows</h3>
             <div>
-                <ul>
-                {
-                    events.map(c => {
-                        return(
-                            <div key={c.id}>
-                                <li>{c.group} - {c.date}</li>  
-                                <button 
-                                    onClick={() => onBuyTickets(c.id)}
-                                >
-                                        Buy Tickets
-                                    </button>
-                            </div>      
-                        )
-                    })
-                }
-                </ul>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Concert</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Location</TableCell>
+                                <TableCell>Venue</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                events.map(c => {
+                                    return(
+                                        <TableRow key={c.id}>
+                                            <TableCell>{c.group}</TableCell>
+                                            <TableCell>{c.date}</TableCell>
+                                            <TableCell>{c.city}, {c.state}</TableCell>
+                                            <TableCell>{c.venue}</TableCell>
+                                            <TableCell>
+                                                <Button 
+                                                    variant="contained"
+                                                    size="small"
+                                                    onClick={() => onBuyTickets(c.id)}
+                                                >
+                                                    Buy Tickets
+                                                </Button>
+                                            </TableCell>
+                                           
+                                        </TableRow>      
+                                    )
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
         </div>
     )
